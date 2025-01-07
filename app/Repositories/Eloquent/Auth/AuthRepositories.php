41ac65f7faa4d4910c1c1e5545f2b9dbd0978b5e
Aaron <aaronjau21@gmail.com>
2025-01-06 20:56:37 -0500

    auth

--- app/Repositories/Eloquent/Auth/AuthRepositories.php
+++ app/Repositories/Eloquent/Auth/AuthRepositories.php
@@ -11,26 +11,30 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthRepositories implements AuthRepositoriesInterface
 {
-  protected $model;
-  public function __construct(User $model)
-  {
-    $this->model = $model;
-  }
-
-  public function login(array $data)
-  {
-    $credentials = request(['email', 'password']);
-
-    if (! $token = Auth::attempt($credentials)) {
-      throw new UnAuthorizeException('Credenciales Incorrectas');
+    protected $model;
+    public function __construct(User $model)
+    {
+        $this->model = $model;
     }
-    $user = $this->model->where('email', $data['email'])->first();
-    if (!$user->confirm_count) throw new  BadRequestException('Falta confirmar el correo, ingrese a su correo');
-    if (!$user->status) throw new  NotPermissionException('No esta permitido');
 
-    return response()->json([
-      'token' => $token,
-      'user' => $user
-    ], 200);
-  }
+    public function login(array $data)
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {
+            throw new UnAuthorizeException('Credenciales Incorrectas');
+        }
+        $user = $this->model->where('email', $data['email'])->first();
+        if (!$user->confirm_count) throw new  BadRequestException('Falta confirmar el correo, ingrese a su correo');
+        if (!$user->status) throw new  NotPermissionException('No esta permitido');
+
+        return response()->json([
+            'token' => $token,
+            'user' => [
+                'name' => $user->full_name,
+                'email' => $user->email,
+                'role' => $user->role,
+            ]
+        ], 200);
+    }
 }
